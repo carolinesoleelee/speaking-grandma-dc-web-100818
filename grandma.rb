596d80b1198777138cbc278326f9dcd4631c7592
Solee Lee <carolinesoleelee@gmail.com>
2018-09-20 04:13:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,17 +1,3 @@
-# Write a speak_to_grandma method.
-def speak_to_grandma(speak)
-  if speak =="Hi!"
-  return "HUH?! SPEAK UP, SONNY!"
-elsif speak == "Hi Nana, how are you?"
-  return "HUH?! SPEAK UP, SONNY!"
-elsif speak == "WHAT DID YOU EAT TODAY?"
-  speak == "WHAT?"
-  return "NO, NOT SINCE 1938!"
-else speak == "I LOVE YOU GRANDMA!"
-  return "I LOVE YOU TOO PUMPKIN!"
- end
-end
-
 
 def speak_to_grandma(speak)
   if speak =="Hi!"
