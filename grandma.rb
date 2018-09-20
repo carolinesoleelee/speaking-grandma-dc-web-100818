27edf44b7913cb10c5d50d9097b81390b08316f6
Solee Lee <carolinesoleelee@gmail.com>
2018-09-20 04:02:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,8 @@
 def speak_to_grandma(speak)
   if speak =="Hi!"
   return "HUH?! SPEAK UP, SONNY!"
-
+elsif speak == "Hi Nana, how are you?"
+  return "HUH?! SPEAK UP, SONNY!"
 else speak == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
  end
