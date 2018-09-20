0e0c1a64d2c0a2afe0a5889e08857d6f04164fcd
Solee Lee <carolinesoleelee@gmail.com>
2018-09-20 04:11:37 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,6 @@ elsif speak == "Hi Nana, how are you?"
 elsif speak == "WHAT DID YOU EAT TODAY?"
   speak == "WHAT?"
   return "NO, NOT SINCE 1938!"
-  return "NO, NOT SINCE 1938!"
 else speak == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
  end
