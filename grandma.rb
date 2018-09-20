2428b0a16cca1bc62e7150097ce1db262c364fc9
Solee Lee <carolinesoleelee@gmail.com>
2018-09-20 04:13:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,18 @@ else speak == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
  end
 end
+
+
+def speak_to_grandma(speak)
+  if speak =="Hi!"
+  return "HUH?! SPEAK UP, SONNY!"
+elsif speak == "Hi Nana, how are you?"
+  return "HUH?! SPEAK UP, SONNY!"
+elsif speak == "WHAT DID YOU EAT TODAY?"
+  return "NO, NOT SINCE 1938!"
+elseif speak == "I LOVE YOU GRANDMA!"
+  return "I LOVE YOU TOO PUMPKIN!"
+else speak == "WHAT?"
+  return "NO, NOT SINCE 1938!"
+ end
+end
