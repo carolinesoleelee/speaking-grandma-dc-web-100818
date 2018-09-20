4265c300dfeccf9895c6c22a56954b1e840f2675
Solee Lee <carolinesoleelee@gmail.com>
2018-09-20 03:43:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(speak)
   if speak == "I LOVE YOU GRANDMA!"
-    speak = "I LOVE YOU TOO PUMPKIN!"
+    return = "I LOVE YOU TOO PUMPKIN!"
 end
 
 
